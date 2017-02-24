728465a8f0be4fd8278d734d01271ff22c7adef4
Sander Shi <sander.hacker@hotmail.com>
2017-02-24 15:25:26 -0500

    children location

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Http, Request, Response } from '@angular/http';
 import { LocationService } from '../rest/location.service';
 import { Location } from '../rest/location';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,18 +15,25 @@ export class DashboardComponent implements OnInit {
   title = 'OpenChirp';
   username = "John Doe";
   location: Location;
+  locations: Array<Location> = [];
   errorMessage: string;
+  childLocations: Array<Location>;
+
   constructor(private locationService: LocationService) {
     document.getElementById("splash-nav").style.display = "none";
     document.getElementById("splash-footer").style.display = "none";
-  }
-
-  myFun() {
-    var self = this;
-    this.locationService.getRootLocation().subscribe(result => this.location = result, error => this.errorMessage = <any>error);
+    this.location = null;
   }
 
   ngOnInit() {
-    this.myFun();
+    this.locationService.getRootLocation().subscribe(result => this.locations.push(result), error => this.errorMessage = error);
+  }
+
+  getChildren(children: Array<string>) {
+    var arrObs: Array<Observable<Location>> = this.locationService.getChildrenLocations(children);
+    var self = this;
+    for (let x of arrObs) {
+      x.subscribe(result => this.childLocations.push(result), error => this.errorMessage = <any>error);
+    }
   }
 }
--- src/app/rest/location.service.ts
+++ src/app/rest/location.service.ts
@@ -1,27 +1,44 @@
-import { Injectable }              from '@angular/core';
-import { Http, Response }          from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import { Location } from './location';
+
 @Injectable()
+
 export class LocationService {
   private locationUrl = 'http://iot.andrew.cmu.edu:10010/api/location';  // URL to web API
-  constructor (private http: Http) {}
+  
+  constructor (private http: Http) {
+
+  }
+  
   getRootLocation (): Observable<Location> {
     return this.http.get(this.locationUrl)
                     .map(this.extractData)
                     .catch(this.handleError);
   }
-  getLocationById(id: string): Observable<Location>{
+
+  getLocationById(id: string): Observable<Location> {
     return this.http.get(this.locationUrl + "/" + id)
                     .map(this.extractData)
                     .catch(this.handleError);
   }
+
+  getChildrenLocations(children: Array<string>): Array<Observable<Location>> {
+    var self = this;
+    var childLocations = children.map(function(x) {
+      return self.http.get(self.locationUrl + "/" + x).map(self.extractData).catch(self.handleError);
+    });
+    return childLocations;
+  }
+
   private extractData(res: Response) {
     let body = res.json();
     return body || { };
   }
+
   private handleError (error: Response | any) {
     // In a real world app, we might use a remote logging infrastructure
     let errMsg: string;
