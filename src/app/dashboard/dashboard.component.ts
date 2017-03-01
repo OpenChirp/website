2792c0d494eeb0bac502e99ddf9b08e87dc972e0
Sander Shi <sander.hacker@hotmail.com>
2017-03-01 18:24:52 -0500

    Trying a component for tree node

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -46,4 +46,5 @@ export class DashboardComponent implements OnInit {
         );
     }
   }
+
 }
---
+++ src/app/tree/tree.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'tree-node',
+    templateUrl: './tree.component.html',
+    styleUrls: ['./tree.component.scss']
+})
+
+export class TreeNode {
+    constructor() {
+        
+    }
+}
