2e7420ab13fe92fdd183db3e8321b7fd409ef22f
Sander Shi <sander.hacker@hotmail.com>
2017-04-11 21:59:36 -0400

    Fix deploy bug.

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,5 +1,5 @@
 import {Component, HostListener, Inject, OnInit} from '@angular/core';
-import { DOCUMENT } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/platform-browser'; 
 
 @Component({
   selector: 'app-splash',
@@ -12,7 +12,7 @@ export class SplashComponent implements OnInit {
   isScrolled = false;
   delta = 100;
 
-  constructor(@Inject(DOCUMENT) private doc: Document) {}
+  constructor(@Inject(DOCUMENT) private doc: any) {}
 
   ngOnInit() {}
 
