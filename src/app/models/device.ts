7495bb6e9771c92b8d59ffa012d4f0044cc647b6
Sander Shi <sander.hacker@hotmail.com>
2017-05-01 11:40:14 -0400

    Services on device page

--- src/app/models/device.ts
+++ src/app/models/device.ts
@@ -4,11 +4,13 @@ export class Device {
     type: string;
     enabled: boolean;
     _id: string;
+    linked_services: Array<Object>;
 
-    constructor(name: string, properties: string, type: string, enabled: boolean) {
+    constructor(name: string, properties: string, type: string, enabled: boolean, linked_services: Array<Object>) {
         this.name = name;
         this.properties = properties;
         this.type = type;
         this.enabled = enabled;
+        this.linked_services = linked_services;
     }
 }
