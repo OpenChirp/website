343a5a44b3bc55558751d779c47175afde3d0ac5
khush-bhatia <khush@cmu.edu>
2017-05-03 21:11:39 -0400

    Resolved merge conflict in newlocation component and removed timeout from error dialog

--- src/app/components/locations/newlocation.component.ts
+++ src/app/components/locations/newlocation.component.ts
@@ -28,14 +28,10 @@ export class NewLocationComponent {
   constructor(private locationService: LocationService, private route: ActivatedRoute,
               private router: Router,
               public snackBar: MdSnackBar,
-<<<<<<< HEAD
               public successDialogService: SuccessDialogService,
               public errorDialogService: ErrorDialogService) {
-=======
-              public dialogService: DialogService) {
     this.name = "";
     this.type = "";
->>>>>>> 40d4760f1770d1ba9746aa1390008c39deaaee16
 
   }
 
@@ -72,12 +68,8 @@ export class NewLocationComponent {
             this.type = "";
           },
           error => {
-<<<<<<< HEAD
             this.errorDialogService
               .dialogPopup(ErrorDialogComponent, error.message + ': ' + this.name);
-=======
-            this.dialogService.dialogPopup(ErrorDialogComponent, this.errorMessage);
->>>>>>> 40d4760f1770d1ba9746aa1390008c39deaaee16
           }
         );
     }
