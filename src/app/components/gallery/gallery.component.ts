67d720f428108b563bfcbbefa7e62ecacdabff8a
Sander Shi <sander.hacker@hotmail.com>
2017-05-22 01:01:17 -0400

    Gallery Temporary fix

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -15,18 +15,18 @@ export class GalleryComponent {
 const base_url = '../../../assets/images/gallery/';
 
 const PICS: Image[] = [
-  // { url: base_url + '1.jpg' },
-  // { url: base_url + '2.jpg' },
+  { url: base_url + '1.jpg' },
+  { url: base_url + '2.jpg' },
   { url: base_url + '3.jpg' },
   { url: base_url + '4.jpg' },
-  // { url: base_url + '5.jpg' },
-  // { url: base_url + '6.jpg' },
-  // { url: base_url + '7.jpg' },
+  { url: base_url + '5.jpg' },
+  { url: base_url + '6.jpg' },
+  { url: base_url + '7.jpg' },
   { url: base_url + '8.jpg' },
   { url: base_url + '9.jpg' },
-  // { url: base_url + '10.jpg' },
+  { url: base_url + '10.jpg' },
   { url: base_url + '11.jpg' },
-  // { url: base_url + '12.jpg' },
+  { url: base_url + '12.jpg' },
 ];
 
 
