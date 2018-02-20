8236cc9583b29b71034de1d24ba1d79154317eb2
Craig Hesling <craig@hesling.com>
2018-02-19 19:52:56 -0500

    Changed config.ts to the more appropriate base_url and grafana_url

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,8 @@
 export class Configuration {
-  base_url: string = "http://iot.andrew.cmu.edu:10010/";
+  base_url: string = "http://openchirp.andrew.cmu.edu:7000/";
   api_url: string = this.base_url + "api/";
   auth_url: string = this.base_url + "auth/";
   google_auth: string = this.auth_url + "google/";
   logout_url: string = this.auth_url + "logout/";
-  grafana_url: string = "http://openchirp.andrew.cmu.edu:3000/";
+  grafana_url: string = "http://openchirp.io:3000/";
 }
