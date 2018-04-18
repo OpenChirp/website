27c41f37b0761bb5d432c5a6e08d593f0b3b40d1
Ivan <r33t@cmu.edu>
2018-04-18 16:36:30 -0400

    Signup button reveals relevant inputs on first click Validate email (clientside only)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,77 +13,79 @@ import { ErrorDialogService } from '../../services/error-dialog.service';
 })
 
 export class LoginComponent {
-  email:string="";
-  name: string = "";
-  password: string = "";
-  confirmPassword: string = "";
-  
-  constructor(private router: Router,
-             private authService: AuthService,
-             private errorDialogService: ErrorDialogService,
-             private successDialogService: SuccessDialogService, 
-             public snackBar: MdSnackBar) {
-   
+  email: string = '';
+  name: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  showSignup: boolean = false;
+  emailPattern: string = '/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/';
 
+  constructor(private router: Router,
+              private authService: AuthService,
+              private errorDialogService: ErrorDialogService,
+              private successDialogService: SuccessDialogService,
+              public snackBar: MdSnackBar) {
   }
 
   ngOnInit() {
   }
 
   login() {
-    if(this.email != "" && this.password != "") {
+    if (this.email != '' && this.password != '') {
       var body: any = {
         username: this.email,
-        password : this.password
+        password: this.password
       };
-   
-        this.authService.basicLogin(body).subscribe(
-            result =>   {
-                //this.router.navigate(['/home'])  
-                window.location.href='/home';                      
-           }, error => {           
-              this.errorDialogService
-                .dialogPopup(error.message );
-            }
-        )}
-     
-    else {
-      this.snackBar.open("Email or password are empty!", "ERROR", { duration: 2000 });
+      this.authService.basicLogin(body).subscribe(
+        result => {
+          //this.router.navigate(['/home'])
+          window.location.href = '/home';
+        }, error => {
+          this.errorDialogService
+            .dialogPopup(error.message);
+        }
+      );
     }
-
+    else {
+      this.snackBar.open('Email or password are empty!', 'ERROR', {duration: 2000});
+    }
   }
 
-  signup(){
-
-    if(this.email != "" && this.password != "") {
-      var body: any = {
-        name: this.name,
-        email: this.email,
-        password : this.password
-      };
-      //TODO: add check for valid email
-      if(this.password === this.confirmPassword){
-        this.authService.signup(body).subscribe(
-          createdUser => {
-            this.successDialogService.dialogPopup("Signup Successful "+this.email).subscribe(
-              result => {
-                  
+  signup() {
+    if (!this.showSignup) {
+      this.showSignup = true;
+    } else {
+      if (this.email != '' && this.password != '') {
+        var body: any = {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        };
+        // TODO : use proper angular FormControl to leverage angular 4 validation and whatnot
+        let regexp = new RegExp(this.emailPattern);
+        if (!regexp.test(this.email)) {
+          this.errorDialogService.dialogPopup('Invalid Email');
+          return;
+        }
+        if (this.password === this.confirmPassword) {
+          this.authService.signup(body).subscribe(
+            createdUser => {
+              this.successDialogService.dialogPopup('Signup Successful ' + this.email).subscribe(
+                result => {
                   this.login();
                 } // Call login to setup session and then redirect to home
-            )},
-          error => {  
-
-              this.errorDialogService.dialogPopup(error.message );
+              )
+            },
+            error => {
+              this.errorDialogService.dialogPopup(error.message);
             }
-        )
+          );
+        } else {
+          this.errorDialogService.dialogPopup('Passwords do not match !');
+        }
+      } else {
+        this.snackBar.open('Email or password are empty!', 'ERROR', {duration: 2000});
       }
-      else{
-          this.errorDialogService.dialogPopup("Paswords dont match !" );
-      }
-    }else {
-      this.snackBar.open("Email or password are empty!", "ERROR", { duration: 2000 });
     }
- }
-  
- 
+  }
 }
