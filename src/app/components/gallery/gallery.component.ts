06abc9f1ba62abd305d66943b4e4bed911abbd90
Ivan <r33t@cmu.edu>
2019-05-20 15:04:48 -0400

    Clear lingering TSLint error
    TODO: Clean up unused components like this one

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -6,30 +6,24 @@ import {Image} from '../../models/image';
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
+
 export class GalleryComponent {
-  public pics = PICS;
+  base_url = '../../../assets/images/gallery/';
+  pics: Image[] = [
+    {url: this.base_url + '1.jpg'},
+    {url: this.base_url + '2.jpg'},
+    {url: this.base_url + '3.jpg'},
+    {url: this.base_url + '4.jpg'},
+    {url: this.base_url + '5.jpg'},
+    {url: this.base_url + '6.jpg'},
+    {url: this.base_url + '7.jpg'},
+    {url: this.base_url + '8.jpg'},
+    {url: this.base_url + '9.jpg'},
+    {url: this.base_url + '10.jpg'},
+    {url: this.base_url + '11.jpg'},
+    {url: this.base_url + '12.jpg'},
+  ];
 
   constructor() {
   };
-
 }
-
-const base_url = '../../../assets/images/gallery/';
-
-const PICS: Image[] = [
-  {url: base_url + '1.jpg'},
-  {url: base_url + '2.jpg'},
-  {url: base_url + '3.jpg'},
-  {url: base_url + '4.jpg'},
-  {url: base_url + '5.jpg'},
-  {url: base_url + '6.jpg'},
-  {url: base_url + '7.jpg'},
-  {url: base_url + '8.jpg'},
-  {url: base_url + '9.jpg'},
-  {url: base_url + '10.jpg'},
-  {url: base_url + '11.jpg'},
-  {url: base_url + '12.jpg'},
-];
-
-
-
